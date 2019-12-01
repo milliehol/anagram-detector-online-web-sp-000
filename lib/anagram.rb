fcf4d0286a47f466d6ad789f98a5b2b99fcea408
Carla Holcomb <carla.programmer@gmail.com>
2019-12-01 22:27:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,16 +3,13 @@ class Anagram
 
   attr_accessor :word
 
-  @@all = []
-
   def initialize(word)
    @word = word
-  @@all << @word.split(" ")
   end
 
   def match(array)
-    array.each do |word|
-    if @@all.sort == word.sort
+    array.select do |word|
+      @@word.sort == word.sort
       return word
     else
       return " "
